78547300a6b7a5d2a4919c2f9e2e8564f9549be5
chiennt198 <51770210+chiennt198@users.noreply.github.com>
2020-11-12 17:17:01 +0700

    aa

--- WebContent/html/js/pages/admin_story_new.js
+++ WebContent/html/js/pages/admin_story_new.js
@@ -14,6 +14,23 @@ var vueItem = new Vue({
     },
     methods: {
     	createStory: function(){
+    		this.error_message = '';
+    		if(!this.storyData.name) {
+    			this.error_message = '<p>Vui lòng nhập tên truyện</p>';
+    		}
+    		if(!this.storyData.authorName) {
+    			this.error_message += '<p>Vui lòng nhập tác giả</p>';
+    		}
+    		if(!this.storyData.description) {
+    			this.error_message += '<p>Vui lòng nhập tóm tắt truyện </p>';
+    		}
+    		
+    		if(!this.storyData.categoryId) {
+    			this.error_message += '<p>Vui lòng chọn thể loại truyện </p>';
+    		}
+    		if(this.error_message) {
+    			return;
+    		}
     		post(this, contextPath + "/admin-regist-story" , {json:JSON.stringify(this.storyData)}, function(data) {
     			if (data.status == STATUS_NORMAL) {
     				alert("Đã tạo truyện mới thành công");
