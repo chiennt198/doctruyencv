be08c3bbce5053e9938ec6127cc5b1bdfe07c318
Admin <Admin@192.168.1.14>
2020-11-25 13:42:04 +0700

    ss

--- WebContent/html/js/pages/doc_truyen.js
+++ WebContent/html/js/pages/doc_truyen.js
@@ -5,6 +5,12 @@ var vueItem = new Vue({
     	chapterInfo:{},
     	storyKey:'',
     	chapterKey:'',
+    	setting:{
+    		fontSize:'20px',
+    		background:'',
+    		color:''
+    	},
+    	config:'0',
     },
     created : function() {
     	
@@ -46,6 +52,56 @@ var vueItem = new Vue({
     	back: function(){
     		window.location.href= contextPath + "/html/truyen.html?storyKey=" + this.storyKey;
     	},
+    	openSetting: function(flg){
+    		this.config = flg;
+    	},
+    	settingFun: function(){
+    		$('.sonsyu').css("font-size",this.setting.fontSize );
+    		$('.sonsyu').css("background-color",this.setting.background );
+    		$('.sonsyu').css("color",this.setting.color );
+    	},
+    	settingColor: function(type, colorType){
+    		
+    		if ( colorType == '1' ) {
+    			if (type == '1') {
+        			this.setting.background = '#f5e4e4';
+        		} else if (type == '2') {
+        			this.setting.background = '#f5ebcd';
+        		} else if (type == '3') {
+        			this.setting.background = '#e2eee2';
+        		} else if (type == '4') {
+        			this.setting.background = '#e1e8e8';
+        		} else if (type == '5') {
+        			this.setting.background = '#eae4d3';
+        		} else if (type == '6') {
+        			this.setting.background = '#e5e3df';
+        		} else if (type == '7') {
+        			this.setting.background = '#222';
+        		} else if (type == '8') {
+        			this.setting.background = '#fff';
+        		}
+    		} else {
+    			if (type == '1') {
+        			this.setting.color = '#f5e4e4';
+        		} else if (type == '2') {
+        			this.setting.color = '#f5ebcd';
+        		} else if (type == '3') {
+        			this.setting.color = '#e2eee2';
+        		} else if (type == '4') {
+        			this.setting.color = '#e1e8e8';
+        		} else if (type == '5') {
+        			this.setting.color = '#eae4d3';
+        		} else if (type == '6') {
+        			this.setting.color = '#e5e3df';
+        		} else if (type == '7') {
+        			this.setting.color = '#222';
+        		} else if (type == '8') {
+        			this.setting.color = '#fff';
+        		}
+    		}
+    		
+    		this.settingFun();
+    	}
     },
 });
 
